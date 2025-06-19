082f76ecf00d6150a9ff9454be4b973dd7e37543
Ankit yadav <ay870421@gmail.com>
2025-06-19 17:50:08 +0530

    chat url fixed

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -13,7 +13,7 @@ import io from "socket.io-client";
 import { useAuth } from "../context/AuthContext";
 import axios from "../api/axios";
 
-const socket = io("http://localhost:8000");
+const socket = io("https://vercel-frontend-lonetown.vercel.app");
 
 const ChatBox = ({ roomId, partnerName }) => {
   const { user } = useAuth();
